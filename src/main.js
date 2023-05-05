81ccb5839d40b89d320ee8a55ec5017e35a515c8
hellomrsasser <131995731+hellomrsasser@users.noreply.github.com>
2023-05-05 12:00:46 -0500

    DONE!

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-document.querySelector("div#content").style["margin-top"] = document.querySelector("div#header").offsetHeight + 10 + "px"; // pushes content div beneath header
-document.querySelectorAll("div#content>img").forEach(i => i.addEventListener("click", function() { // turns imgs into hyperlinks to full-size image
+function updateSpacing() {
+  document.querySelector("div#content").style["margin-top"] = document.querySelector("div#header").offsetHeight + 10 + "px";
+}
+
+updateSpacing();
+setInterval(updateSpacing, 250); // pulls content beneath header at regular intervals
+
+document.querySelectorAll("div#content>img:not([data-pdf-src])").forEach(i => i.addEventListener("click", function() { // turns imgs into hyperlinks to full-size image
   let a = document.createElement("a");
   a.href = i.src;
+  a.target = "_blank";
+  a.click();
+}));
+
+document.querySelectorAll("div#content>img[data-pdf-src]").forEach(i => i.addEventListener("click", function() {
+  let pdf = i.getAttribute("data-pdf-src");
+  console.log(pdf);
+  let a = document.createElement("a");
+  a.href = pdf;
+  a.download = pdf.slice(pdf.lastIndexOf("/") + 1);
+  a.target = "_blank";
   a.click();
 }));
